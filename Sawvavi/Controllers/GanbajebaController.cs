1e225237cb334dff8217058b0be648c1d3fe646b
Beka Kublashvili <bekakubla@gmail.com>
2021-04-01 16:59:38 +0400

    GanbajebaV2

--- Sawvavi/Controllers/GanbajebaController.cs
+++ Sawvavi/Controllers/GanbajebaController.cs
@@ -9,30 +9,96 @@ namespace FuelProject.Controllers
 {
     public class GanbajebaController:Controller
     {
-        [HttpGet]
-        public IActionResult Index()
+        private void SizeYear()
         {
             var a = 2021;
-            var b = 0.1;
+            double b = 0.0;
             var yearList = new List<int>();
-            var sizeList = new List<double>();
+            var sizeList = new List<string>();
             for (int i = 0; i < 57; i++)
             {
                 yearList.Add(a - i);
             }
-            ViewBag.yearList = yearList;
-            for (var i = 0;i<63;i++)
+            for (var i = 0; i < 63; i++)
             {
-                sizeList.Add(b+0.1);
                 b = b + 0.1;
+                var c = string.Format("{0:0.0}", b);
+                sizeList.Add(c);
             }
+            ViewBag.yearList = yearList;
             ViewBag.sizeList = sizeList;
+        }
+        private static void Aqcizi(Ganbajeba ganbajeba)
+        {
+            if (ganbajeba.Year >= 2019 || ganbajeba.Year == 2009)
+            {
+                ganbajeba.Aqcizi = 1.5 * Convert.ToDouble(ganbajeba.Size) * 1000;
+                ganbajeba.AqciziInString = string.Format("{0:0}", ganbajeba.Aqcizi);
 
-            //ViewBag.sizeList = new List<double>() { 0.1, 0.2, 0.3, 0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,
-            //                                        2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9,
-            //                                        4.0,4.1,4.2,4.3,4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6,5.7,5.8,5.9,
-            //                                        6.0,6.1,6.2,6.3};
-
+            }
+            else if (ganbajeba.Year >= 2013 && ganbajeba.Year <= 2015)
+            {
+                ganbajeba.Aqcizi = 0.8 * Convert.ToDouble(ganbajeba.Size) * 1000;
+                ganbajeba.AqciziInString = string.Format("{0:0}", ganbajeba.Aqcizi);
+            }
+            else if (ganbajeba.Year == 2011)
+            {
+                ganbajeba.Aqcizi = 1.1 * Convert.ToDouble(ganbajeba.Size) * 1000;
+                ganbajeba.AqciziInString = string.Format("{0:0}", ganbajeba.Aqcizi);
+            }
+            else if (ganbajeba.Year == 2017)
+            {
+                ganbajeba.Aqcizi = 1.2 * Convert.ToDouble(ganbajeba.Size) * 1000;
+                ganbajeba.AqciziInString = string.Format("{0:0}", ganbajeba.Aqcizi);
+            }
+            else if (ganbajeba.Year == 2010)
+            {
+                ganbajeba.Aqcizi = 1.3 * Convert.ToDouble(ganbajeba.Size) * 1000;
+                ganbajeba.AqciziInString = string.Format("{0:0}", ganbajeba.Aqcizi);
+            }
+            else if (ganbajeba.Year == 2018)
+            {
+                ganbajeba.Aqcizi = 1.4 * Convert.ToDouble(ganbajeba.Size) * 1000;
+                ganbajeba.AqciziInString = string.Format("{0:0}", ganbajeba.Aqcizi);
+            }
+            else if (ganbajeba.Year == 2008)
+            {
+                ganbajeba.Aqcizi = 1.8 * Convert.ToDouble(ganbajeba.Size) * 1000;
+                ganbajeba.AqciziInString = string.Format("{0:0}", ganbajeba.Aqcizi);
+            }
+            else if (ganbajeba.Year == 2007)
+            {
+                ganbajeba.Aqcizi = 2.1 * Convert.ToDouble(ganbajeba.Size) * 1000;
+                ganbajeba.AqciziInString = string.Format("{0:0}", ganbajeba.Aqcizi);
+            }
+            else if (ganbajeba.Year == 2016)
+            {
+                ganbajeba.Aqcizi = 1.0 * Convert.ToDouble(ganbajeba.Size) * 1000;
+                ganbajeba.AqciziInString = string.Format("{0:0}", ganbajeba.Aqcizi);
+            }
+            else
+            {
+                ganbajeba.Aqcizi = 2.4 * Convert.ToDouble(ganbajeba.Size) * 1000;
+                ganbajeba.AqciziInString = string.Format("{0:0}", ganbajeba.Aqcizi);
+            }
+        }
+        private static void Gadasaxadi(Ganbajeba ganbajeba)
+        {
+            if (ganbajeba.Year == 2021)
+            {
+                ganbajeba.Gadasaxadi = (Convert.ToDouble(ganbajeba.Size) * 1000 * 0.05);
+            }
+            else
+            {
+                int dateNow = 2021;
+                ganbajeba.Gadasaxadi = (Convert.ToDouble(ganbajeba.Size) * 1000 * 0.05) + (Convert.ToDouble(ganbajeba.Size) * 1000 * (dateNow - ganbajeba.Year) * 0.0025);
+                ganbajeba.GadasaxadiInString=string.Format("{0:0}", ganbajeba.Gadasaxadi);
+            }
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            SizeYear();
             return View();
         }
         [HttpPost]
@@ -40,16 +106,11 @@ namespace FuelProject.Controllers
         {
             if (ModelState.IsValid)
             {
-                ganbajeba.Sum = ganbajeba.Size + ganbajeba.Year;
+                Aqcizi(ganbajeba);
+                Gadasaxadi(ganbajeba);
+                return RedirectToAction("Result");
             }
-            ViewBag.yearList = new List<int>() { 1965,1966,1967,1968,1969,1970,1971,1972,1973,1974,1975,1976,1977,1978,1979,1980,
-                                                 1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,
-                                                 1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,
-                                                 2013,2014,2015,2016,2017,2018,2019,2020,2021};
-            ViewBag.sizeList = new List<double>() { 0.1, 0.2, 0.3, 0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,
-                                                    2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9,
-                                                    4.0,4.1,4.2,4.3,4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6,5.7,5.8,5.9,
-                                                    6.0,6.1,6.2,6.3};
+            SizeYear();
             return View();
         }
     }
--- Sawvavi/Models/Ganbajeba.cs
+++ Sawvavi/Models/Ganbajeba.cs
@@ -8,7 +8,10 @@ namespace FuelProject.Models
     public class Ganbajeba
     {
         public int Year { get; set; }
-        public double Size { get; set; }
-        public double Sum { get; set; }
+        public string Size { get; set; }
+        public double Aqcizi { get; set; }
+        public double Gadasaxadi { get; set; }
+        public string AqciziInString { get; set; }
+        public string GadasaxadiInString { get; set; }
     }
 }
